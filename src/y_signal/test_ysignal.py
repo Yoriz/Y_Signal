a8ab8ebfcf6b82381708460471240316147b8572
Yoriz <dave.wilson1@ntlworld.com>
2013-07-31 23:06:29 +0100

    pep8 tidying

--- src/y_signal/test_ysignal.py
+++ src/y_signal/test_ysignal.py
@@ -4,7 +4,7 @@ Created on 27 Mar 2013
 @author: Dave Wilson
 '''
 
-from y_signal.ysignal import Ysignal
+import y_signal.ysignal as ysignal
 import time
 import unittest
 
@@ -12,7 +12,7 @@ import unittest
 class TestYsignal(unittest.TestCase):
 
     def setUp(self):
-        self.signal = Ysignal(True)
+        self.signal = ysignal.Ysignal(True)
         self.attr1 = None
         self.attr2 = None
         self.attr3 = 0
@@ -20,126 +20,128 @@ class TestYsignal(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def setAttr1(self, value):
+    def set_attr1(self, value):
         self.attr1 = value
 
-    def methodForTest(self):
+    def method_for_test(self):
         pass
 
-    def methodAdd(self):
+    def method_add(self):
         time.sleep(0.05)
         self.attr3 += 1
 
-    def testConnectFunction(self):
-        def testFunction():
+    def test_connect_function(self):
+        def test_function():
             pass
-        func = testFunction
+        func = test_function
         self.signal.bind(func)
-        self.signal.waitInQueue()
+        self.signal.wait_in_queue()
         self.assertSetEqual(set((func,)), self.signal._functions)
 
-    def testConnectMethod(self):
-        self.signal.bind(self.setAttr1)
-        self.signal.waitInQueue()
+    def test_connect_method(self):
+        self.signal.bind(self.set_attr1)
+        self.signal.wait_in_queue()
         self.assertIn(self, self.signal._methods)
         funcs = self.signal._methods.get(self, set())
-        self.assertIn(self.setAttr1.__func__, funcs)
+        self.assertIn(self.set_attr1.__func__, funcs)
 
-    def testEmitFunction(self):
+    def test_emit_function(self):
         value = 'EmitFunction'
 
-        def testFunction(value):
+        def test_function(value):
             self.attr1 = value
-        self.signal.bind(testFunction)
+
+        self.signal.bind(test_function)
         self.signal.emit(value=value)
-        self.signal.waitInQueue()
+        self.signal.wait_in_queue()
         self.assertEqual(self.attr1, value)
 
-    def testEmitMethod(self):
+    def test_emit_method(self):
         value = 'EmitMethod'
-        self.signal.bind(self.setAttr1)
+        self.signal.bind(self.set_attr1)
         self.signal.emit(value=value)
-        self.signal.waitInQueue()
+        self.signal.wait_in_queue()
         self.assertEqual(self.attr1, value)
 
-    def testEmitFunctionAndMethod(self):
+    def test_emit_function_and_method(self):
         value = 'EmitBothTypes'
 
-        def testFunction(value):
+        def test_function(value):
             self.attr2 = value
-        self.signal.bind(testFunction)
-        self.signal.bind(self.setAttr1)
+
+        self.signal.bind(test_function)
+        self.signal.bind(self.set_attr1)
         self.signal.emit(value=value)
-        self.signal.waitInQueue()
+        self.signal.wait_in_queue()
         self.assertEqual(self.attr1, value)
         self.assertEqual(self.attr2, value)
 
-    def testEmitSlotFunction(self):
+    def test_emit_slot_function(self):
         value = 'EmitSlotFunction'
 
-        def testFunction(value):
+        def test_function(value):
             self.attr1 = value
 
-        self.signal.emitSlot(testFunction, value=value)
-        self.signal.waitInQueue()
+        self.signal.emit_slot(test_function, value=value)
+        self.signal.wait_in_queue()
         self.assertEqual(self.attr1, value)
 
-    def testEmitSlotMethod(self):
+    def test_emit_slot_method(self):
         value = 'EmitMethod'
-        self.signal.emitSlot(self.setAttr1, value=value)
-        self.signal.waitInQueue()
+        self.signal.emit_slot(self.set_attr1, value=value)
+        self.signal.wait_in_queue()
         self.assertEqual(self.attr1, value)
 
-    def testWaitInQueue(self):
-        self.signal.emitSlot(self.methodAdd)
-        self.signal.waitInQueue()
+    def test_wait_in_queue(self):
+        self.signal.emit_slot(self.method_add)
+        self.signal.wait_in_queue()
         self.assertEqual(self.attr3, 1)
 
-    def testDisconnectSlotFunction(self):
-        def testFunction():
+    def test_disconnect_slot_function(self):
+        def test_function():
             pass
 
-        def testFunction2():
+        def test_function2():
             pass
 
-        func = testFunction
-        func2 = testFunction2
+        func = test_function
+        func2 = test_function2
         self.signal.bind(func)
         self.signal.bind(func2)
-        self.signal.waitInQueue()
+        self.signal.wait_in_queue()
         self.assertSetEqual(set((func, func2)), self.signal._functions)
         self.signal.unbind(func2)
-        self.signal.waitInQueue()
+        self.signal.wait_in_queue()
         self.assertSetEqual(set((func,)), self.signal._functions)
 
-    def testDisconnectSlotMethod(self):
-        self.signal.bind(self.setAttr1)
-        self.signal.bind(self.methodForTest)
-        self.signal.waitInQueue()
+    def test_disconnect_slot_method(self):
+        self.signal.bind(self.set_attr1)
+        self.signal.bind(self.method_for_test)
+        self.signal.wait_in_queue()
         self.assertIn(self, self.signal._methods)
         funcs = self.signal._methods.get(self, [])
-        self.assertSetEqual(set((self.setAttr1.__func__,
-                                self.methodForTest.__func__)), funcs)
-        self.signal.unbind(self.methodForTest)
-        self.signal.waitInQueue()
+        self.assertSetEqual(set((self.set_attr1.__func__,
+                                self.method_for_test.__func__)), funcs)
+        self.signal.unbind(self.method_for_test)
+        self.signal.wait_in_queue()
         funcs = self.signal._methods.get(self, set())
-        self.assertSetEqual(set((self.setAttr1.__func__,)), funcs)
+        self.assertSetEqual(set((self.set_attr1.__func__,)), funcs)
 
-    def testDisconnectAll(self):
-        def testFunction():
+    def test_disconnect_all(self):
+        def test_function():
             pass
 
-        def testFunction2():
+        def test_function2():
             pass
 
-        func = testFunction
-        func2 = testFunction2
+        func = test_function
+        func2 = test_function2
         self.signal.bind(func)
         self.signal.bind(func2)
-        self.signal.bind(self.setAttr1)
-        self.signal.bind(self.methodForTest)
-        self.signal.unbindAll()
-        self.signal.waitInQueue()
+        self.signal.bind(self.set_attr1)
+        self.signal.bind(self.method_for_test)
+        self.signal.unbind_all()
+        self.signal.wait_in_queue()
         self.assertSetEqual(set(), self.signal._functions)
         funcs = self.signal._methods.get(self, set())
         self.assertSetEqual(set(), funcs)
--- src/y_signal/ysignal.py
+++ src/y_signal/ysignal.py
@@ -4,118 +4,90 @@ Created on 28 Jan 2013
 @author: Dave Wilson
 '''
 
-from weakref import WeakSet, WeakKeyDictionary
+import Queue as queue
+import collections
 import inspect
-from Queue import Queue, Empty
-from threading import Thread, Lock
+import threading
+import weakref
+
+worker = collections.namedtuple('Worker', 'func args kwargs')
 
 
-class _ThreadWorker(object):
-    def __init__(self, func, *args, **kwargs):
-        self.class_ = func
-        self.args = args
-        self.kwargs = kwargs
-
-
-class QThread(Thread):
+class QThread(threading.Thread):
     '''
-    A thread that that picks off and calls items from the passed in queue until
+    A thread that picks off and calls items from the passed in queue until
     there are no items left
     '''
-    def __init__(self, queue, lock):
-        '''
-
-        :param queue: An instance of a QueuedThread
-        :param lock: The thread lock to use with this thread
-        '''
-        Thread.__init__(self)
+    def __init__(self, queued_thread):
+        threading.Thread.__init__(self)
         self.daemon = False
-        self.queue = queue
-        self.lock = lock
+        self.queue = queued_thread
         self.start()
 
     def run(self):
         self.queue.threadRunning = True
         while not self.queue.empty():
-            with self.lock:
-                try:
-                    item = self.queue.get_nowait()
-                    item.class_(*item.args, **item.kwargs)
-                    self.queue.task_done()
-                except Empty:
-                    pass
+            try:
+                item = self.queue.get_nowait()
+                item.func(*item.args, **item.kwargs)
+                self.queue.task_done()
+            except queue.Empty:
+                pass
         self.queue.threadRunning = False
 
 
-class QueuedThread(Queue):
+class QueuedThread(queue.Queue):
     def __init__(self):
-        Queue.__init__(self)
+        queue.Queue.__init__(self)
         self.threadRunning = False
 
-    def createThread(self):
-        self.thread = QThread(self, Lock())
-
     def submit(self, func, *args, **kwargs):
-        self.put(_ThreadWorker(func, *args, **kwargs))
+        self.put(worker(func, args, kwargs))
         if not self.threadRunning:
-            self.createThread()
+            QThread(self)
 
     def waitThreadNotRunning(self):
         self.join()
 
 
-queuedThread = QueuedThread()
+queued_thread = QueuedThread()
 
 
 class Ysignal(object):
-    '''WeakRef Signal/Slots that uses a single threaded pool if required'''
-    def __init__(self, useThread=True):
+    '''WeakRef Signal/Slots that uses a queued thread if required'''
+    def __init__(self, use_thread=True):
         '''Initialise attributes to store observers'''
-        self._functions = WeakSet()
-        self._methods = WeakKeyDictionary()
-        self.useThread = useThread
-        self.queuedThread = queuedThread
+        self._functions = weakref.WeakSet()
+        self._methods = weakref.WeakKeyDictionary()
+        self.use_thread = use_thread
+        self.queued_thread = queued_thread
 
-    def slotCheck(self, future):
-        '''Raises an exception if an error occurred during the future call'''
-        future.result()
-
-    def waitInQueue(self):
+    def wait_in_queue(self):
         '''Wait in queue for signals to complete'''
-        self.queuedThread.waitThreadNotRunning()
+        self.queued_thread.waitThreadNotRunning()
 
-    def shutDownQueue(self):
-        self.queuedThread.shutdown()
-
-#     def waitTillQueueEmpty(self):
-#         '''Wait till the queue is empty (not reliable!)'''
-#         while not self.queuedThread._work_queue.empty():
-#             print 'Waiting in waitTillEndQueue'
-#             self.waitInQueue()
-#         self.waitInQueue()
-
-    def emitSlot(self, slot, *args, **kwargs):
+    def emit_slot(self, slot, *args, **kwargs):
         '''emit a signal to the passed in slot only'''
-        if self.useThread:
-            self.queuedThread.submit(slot, *args, **kwargs)
+        if self.use_thread:
+            self.queued_thread.submit(slot, *args, **kwargs)
 
         else:
             slot(*args, **kwargs)
 
     def emit(self, *args, **kwargs):
         '''emit a signal to all slots'''
-        if self.useThread:
-            self.queuedThread.submit(self._emitCall, *args, **kwargs)
+        if self.use_thread:
+            self.queued_thread.submit(self._emit_call, *args, **kwargs)
 
         else:
-            self._emitCall(*args, **kwargs)
+            self._emit_call(*args, **kwargs)
 
-    def _emitCall(self, *args, **kwargs):
+    def _emit_call(self, *args, **kwargs):
         '''Emit a signal to all slots'''
-        self._emitFunctions(*args, **kwargs)
+        self._emit_functions(*args, **kwargs)
         self._emitMethods(*args, **kwargs)
 
-    def _emitFunctions(self, *args, **kwargs):
+    def _emit_functions(self, *args, **kwargs):
         '''Emits a signal to any Function slots'''
         for func in self._functions:
             func(*args, **kwargs)
@@ -129,69 +101,69 @@ class Ysignal(object):
 
     def bind(self, slot):
         '''Add a slot to the list of listeners'''
-        if self.useThread:
-            self.queuedThread.submit(self._bindCall, slot)
+        if self.use_thread:
+            self.queued_thread.submit(self._bind_call, slot)
 
         else:
-            self._bindCall(slot)
+            self._bind_call(slot)
 
-    def _bindCall(self, slot):
+    def _bind_call(self, slot):
         '''Add a slot to the list of listeners'''
         if inspect.ismethod(slot):
-            self._bindMethod(slot)
+            self._bind_method(slot)
         else:
-            self._bindFunction(slot)
+            self._bind_function(slot)
 
-    def _bindFunction(self, slot):
+    def _bind_function(self, slot):
         '''Add a Function slot'''
         self._functions.add(slot)
 
-    def _bindMethod(self, slot):
+    def _bind_method(self, slot):
         '''Add a Method slot'''
         try:
             self._methods[slot.__self__].add(slot.__func__)
         except KeyError:
             self._methods[slot.__self__] = set()
-            self._bindMethod(slot)
+            self._bind_method(slot)
 
     def unbind(self, slot):
         '''Remove slot from the list of listeners'''
-        if self.useThread:
-            self.queuedThread.submit(self._unbindCall, slot)
+        if self.use_thread:
+            self.queued_thread.submit(self._unbind_call, slot)
 
         else:
-            self._unbindCall(slot)
+            self._unbind_call(slot)
 
-    def _unbindCall(self, slot):
+    def _unbind_call(self, slot):
         '''Remove slot from the list of listeners'''
         if inspect.ismethod(slot):
-            self._unbindMethod(slot)
+            self._unbind_method(slot)
         else:
-            self._unbindFunction(slot)
+            self._unbind_function(slot)
 
-    def _unbindFunction(self, slot):
+    def _unbind_function(self, slot):
         '''Remove a Function slot'''
         try:
             self._functions.remove(slot)
         except ValueError:
             pass
 
-    def _unbindMethod(self, slot):
+    def _unbind_method(self, slot):
         '''Remove a Method slot'''
         try:
             self._methods[slot.__self__].remove(slot.__func__)
         except (ValueError, KeyError):
             pass
 
-    def unbindAll(self):
+    def unbind_all(self):
         '''Remove all slots'''
-        if self.useThread:
-            self.queuedThread.submit(self._unbindAllCall)
+        if self.use_thread:
+            self.queued_thread.submit(self._unbind_all_call)
 
         else:
-            self._unbindAllCall()
+            self._unbind_all_call()
 
-    def _unbindAllCall(self):
+    def _unbind_all_call(self):
         '''Remove all slots'''
         self._functions.clear()
         self._methods.clear()
